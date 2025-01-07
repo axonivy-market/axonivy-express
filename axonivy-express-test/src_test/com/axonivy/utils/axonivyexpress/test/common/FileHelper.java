19ad42802d1c881f1a2d51dd3678166835fc06e5
mnhnam-axonivy <nam.mai@axonactive.com>
2025-01-07 17:28:48 +0700

    IVYPORTAL-16522 Portal Express - Make integration via non specific customizations - Updated test files

--- axonivy-express-test/src_test/com/axonivy/utils/axonivyexpress/test/common/FileHelper.java
+++ axonivy-express-test/src_test/com/axonivy/utils/axonivyexpress/test/common/FileHelper.java
@@ -2,7 +2,7 @@ package com.axonivy.utils.axonivyexpress.test.common;
 
 public class FileHelper {
   public static String getAbsolutePathToTestFile(String fileName) {
-    return System.getProperty("user.dir") + "\\resources\\testFile\\"
+    return System.getProperty("user.dir") + "//resources//testFile//"
         + fileName;
   }
 }
